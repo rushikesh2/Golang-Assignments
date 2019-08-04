f6bf4cb913de3736d2c72a25f217cb344cd3df1b
rushikesh2 <gurav41rushikesh@gmail.com>
2019-08-04 15:27:20 +0530

    basic code

---
+++ _03_variables/01_shortand/main1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10
+	b := "golang"
+	c := 4.17
+	d := true
+	e := 'M'
+
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+	fmt.Printf("%T \n", e)
+}
---
+++ _03_variables/02_var_zero_value/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+//print default value of below variables
+func main() {
+
+	var a int
+	var b string
+	var c float64
+	var d bool
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+	fmt.Println()
+}
