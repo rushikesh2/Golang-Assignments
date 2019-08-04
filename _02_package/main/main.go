0309a0c34c44a620da3eb73548a075703c279d67
rushikesh2 <gurav41rushikesh@gmail.com>
2019-08-04 13:18:27 +0530

    my second program

---
+++ _02_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "github.com/rushikesh2/GolangTraining/_02_package/stringutill"
+)
+
+func main() {
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
+    fmt.Println(stringutil.MyName)
+}
